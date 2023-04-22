68c6576bb9caca61ef9640c234ab6563df02b146
so_val <soyuz108@gmail.com>
2023-04-22 16:50:25 -0600

    Factory function finished

--- mutating_data.js
+++ mutating_data.js
@@ -15,20 +15,69 @@ const mockUpStrand = () => {
 
 // Returns an organism with a mutated DNA
 const pAequorFactory = (specimenNum, dna) => {
-    const organism = {
-      specimenNum,
-      dna,
+  return {
+    specimenNum,
+    dna,
     //mutates one random DNA base to a different base
-      mutate() {
-        for (i in dna) {
-          let oldBase = dna[i];
-          while (dna[i] === oldBase) {
-            dna[i] = returnRandBase();
-          }
+    mutate() {
+      let randomIndex = [Math.floor(Math.random() * this.dna.length)]; // get a random index of the current base to mutate
+      let newBase;
+      do {
+        newBase = returnRandBase();
+      } while (this.dna[randomIndex] === newBase); // replace with a random rand base if they are the same
+
+      return this.dna;
+    },
+    // compares a new DNA with the old one and finds a percentage of the difference
+    compareDNA(pAequorObj) {
+      let matchCount = 0;
+      for (i = 0; i < this.dna.length; i++) {
+        if (pAequorObj.dna[i] === this.dna[i]) {
+          matchCount++;
         }
-        return this.dna;
       }
-    }
-    return organism;
+      let percentageMatch = ((matchCount / this.dna.length) * 100).toFixed(2);
+      console.log(
+        `Specimen #${this._specimenNum} and specimen #${pAequorObj._specimenNum} have ${percentageMatch}% DNA in common.`
+      );
+      return percentageMatch;
+    },
+    // returns true if at least 60% bases are C or G
+    willLikelySurvive() {
+      let count = 0;
+      //
+      this.dna.forEach((element) => {
+        if (element === "C" || element === "G") {
+          count++;
+        }
+      });
+      if (count / this.dna.length >= 0.6) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    // method to return the complementary DNA strand( A <-> T, C <-> G)
+    complementStrand() {
+      let complStrand = [];
+      for (let base of this.dna) {
+        switch (base) {
+          case "A":
+            compStrand.push("T");
+            break;
+          case "T":
+            compStrand.push("A");
+            break;
+          case "C":
+            compStrand.push("G");
+            break;
+          case "G":
+            compStrand.push("C");
+            break;
+        }
+      }
+      return complStrand;
+    },
+  };
 };
 console.log(pAequorFactory(1, mockUpStrand()));
