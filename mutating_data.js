5c844534baa16203d175b321afd53371e3bb1e02
so_val <soyuz108@gmail.com>
2023-04-22 18:31:47 -0600

    Finished a pool and added tests

--- mutating_data.js
+++ mutating_data.js
@@ -18,14 +18,14 @@ const pAequorFactory = (specimenNum, dna) => {
   return {
     specimenNum,
     dna,
-    //mutates one random DNA base to a different base
+    // mutates one random DNA base to a different base
     mutate() {
-      let randomIndex = [Math.floor(Math.random() * this.dna.length)]; // get a random index of the current base to mutate
+      let randomIndex = Math.floor(Math.random() * this.dna.length); // get a random index of the current base to mutate
       let newBase;
       do {
         newBase = returnRandBase();
       } while (this.dna[randomIndex] === newBase); // replace with a random rand base if they are the same
-
+      dna[randomIndex] = newBase;
       return this.dna;
     },
     // compares a new DNA with the old one and finds a percentage of the difference
@@ -63,16 +63,16 @@ const pAequorFactory = (specimenNum, dna) => {
       for (let base of this.dna) {
         switch (base) {
           case "A":
-            compStrand.push("T");
+            complStrand.push("T");
             break;
           case "T":
-            compStrand.push("A");
+            complStrand.push("A");
             break;
           case "C":
-            compStrand.push("G");
+            complStrand.push("G");
             break;
           case "G":
-            compStrand.push("C");
+            complStrand.push("C");
             break;
         }
       }
@@ -80,4 +80,33 @@ const pAequorFactory = (specimenNum, dna) => {
     },
   };
 };
-console.log(pAequorFactory(1, mockUpStrand()));
+// Tests the factory function
+// console.log(pAequorFactory(1, mockUpStrand()));
+
+// Checks the mutate method
+let newArray = mockUpStrand();
+console.log(newArray);
+let mutatedTest = pAequorFactory(1, newArray).mutate();
+console.log(mutatedTest);
+
+// Checks the complementStrand method
+let strand = mockUpStrand();
+let complementTest = pAequorFactory(1, strand).complementStrand();
+console.log(strand);
+console.log(complementTest);
+
+// Creates 30 specimen that can survive (at least 60% of the DNA strand must be C's of G's)
+const poolOf30 = () => {
+  let likelySurvivors = [];
+  while (likelySurvivors.length < 30) {
+    let i = 1;
+    let sample = pAequorFactory(i, mockUpStrand());
+    if (sample.willLikelySurvive()) {
+      likelySurvivors.push(sample.dna);
+    }
+    i++;
+  }
+  return likelySurvivors;
+};
+// Tests the pool function
+// console.log(poolOf30());
