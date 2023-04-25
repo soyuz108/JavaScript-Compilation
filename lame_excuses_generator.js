c1aac496862d455382a8f1a5d8a37f8ebbfda3bc
so_val <soyuz108@gmail.com>
2023-04-24 18:13:28 -0600

    Add new project

---
+++ lame_excuses_generator.js
@@ -0,0 +1,41 @@
+// Store random data in an object
+const randomData = {
+  intro: [
+    "Sorry I can't come",
+    "Please forgive my absence",
+    "This is going to sound crazy",
+    "Get this:",
+    "I can't go because",
+    "I know you're going to hate me but",
+    "I was minding my own business and boom!",
+    "I feel terrible but",
+    "I regretfully cannot attend,",
+    "This is going to sound like an excuse but",
+  ],
+  scapegoat: [
+    "my nephew",
+    "the ghost of Hitler",
+    "the Pope",
+    "my ex",
+    "a high school marching band",
+    "Taylor Swift",
+    "a sad clown",
+    "the kid from Disney",
+    "a professional cricket team",
+    "my Tinder date",
+  ],
+  delay: [
+    "just shit the bed",
+    "died in front of me",
+    "won't stop telling me knock knock jokes",
+    "is having a nervous breakdown",
+    "gave me syphilis",
+    "poured lemonade in my tank",
+    "stabbed me",
+    "found my bpx of human teeth",
+    "stole my bicycle",
+    "posted my Instagram",
+  ],
+};
+
+// 
