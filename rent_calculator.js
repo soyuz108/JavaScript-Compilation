c3a6fda9da0e7c21cd966d2bed1a430837cf1181
so_val <soyuz108@gmail.com>
2023-10-08 22:53:39 -0600

    Added a challenge from Codewars

---
+++ rent_calculator.js
@@ -0,0 +1,38 @@
+/* Every day you rent the car costs $40. 
+If you rent the car for 7 or more days, you get $50 off your total. 
+Alternatively, if you rent the car for 3 or more days, you get $20 off your total. 
+The function must return the total amount for different days.*/
+
+function rentalCarCost(d) {
+  const price = 40;
+  const total = price * d;
+  if (d >= 7) {
+    return total - 50;
+  } else if (d >= 3) {
+    return total - 20;
+  } else {
+    return total;
+  }
+}
+
+// Valid tests
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Regular Tests:", function () {
+  it("Should work for under 3 days:", function () {
+    assert.strictEqual(rentalCarCost(1), 40);
+    assert.strictEqual(rentalCarCost(2), 80);
+  });
+
+  it("Should work for under 7 days:", function () {
+    assert.strictEqual(rentalCarCost(3), 100);
+    assert.strictEqual(rentalCarCost(6), 220);
+  });
+
+  it("Should work for 7 or more days:", function () {
+    assert.strictEqual(rentalCarCost(9), 310);
+    assert.strictEqual(rentalCarCost(10), 350);
+  });
+});
