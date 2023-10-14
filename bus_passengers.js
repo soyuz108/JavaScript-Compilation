20253a460dfbca95bcd1d9bbc6740de6c7d48cb7
so_val <soyuz108@gmail.com>
2023-10-13 20:03:36 -0600

    Added a new challenge

---
+++ bus_passengers.js
@@ -0,0 +1,25 @@
+/* There is a bus moving in the city which takes and drops some people at each bus stop.
+You are provided with a list (or array) of integer pairs. 
+Elements of each pair represent the number of people that get on the bus (the first item) and get off the bus (the second item) at a bus stop.
+Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). 
+Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus. Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+The second value in the first pair in the array is 0, since the bus is empty in the first bus stop. */
+
+// Classic function
+
+function number(busStops) {
+  let total = 0;
+  for (let i = 0; i < busStops.length; i++) {
+    total += busStops[i][0];
+    total -= busStops[i][1];
+  }
+  return total;
+}
+
+// Testing cases
+console.log(number([[10,0],[3,5],[5,8]])); // 5
+console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]])); // 21
+console.log(number([[0, 0]])); // 0
+
+// One line function
+const number = busStops => busStops.reduce((n, [on, off]) => n + on - off, 0);
