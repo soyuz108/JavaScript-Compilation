86ade02150cfcc62fbb863d1ad674834864809c1
so_val <soyuz108@gmail.com>
2023-10-25 20:41:55 -0600

    Added a simplified conditional variant

--- who_like_it.js
+++ who_like_it.js
@@ -23,3 +23,14 @@ console.log(get_Likes(["Peter"])); // Peter likes this
 console.log(get_Likes(["Jacob", "Alex"])); // Jacob and Alex like this
 console.log(get_Likes(["Max", "John", "Mark"])); // Max, John and Mark like this
 console.log(get_Likes(["Alex", "Jacob", "Mark", "Max"])); // Alex, Jacob and 2 others like this
+
+// Simplified conditional
+function get_Likes(names) {
+  return {
+    0: "No one likes this",
+    1: `${names[0]} likes this`,
+    2: `${names[0]} and ${names[1]} like this`,
+    3: `${names[0]}, ${names[1]} and ${names[2]} like this`,
+    4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+  } [Math.min(4, names.length)];
+}
