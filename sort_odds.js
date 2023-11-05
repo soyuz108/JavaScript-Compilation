a5e5244cbce4c5138372272270fab176d5e5c481
so_val <soyuz108@gmail.com>
2023-11-04 22:07:53 -0600

    Added a refactored variant

--- sort_odds.js
+++ sort_odds.js
@@ -33,3 +33,10 @@ console.log(sortArray([5, 3, 2, 8, 1, 4])); // [ 1, 3, 2, 8, 5, 4 ]
 console.log(sortArray([])); // []
 console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])); // [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 console.log(sortArray([7, 1])); // [ 1, 7 ]
+
+// Simplified function
+function sortArray(arr) {
+  // number % 2 is 0 (so False) if number is even
+  const oddArr = arr.filter((x) => x % 2).sort((a, b) => a - b);
+  return arr.map((x) => (x % 2 ? oddArr.shift() : x));
+}
