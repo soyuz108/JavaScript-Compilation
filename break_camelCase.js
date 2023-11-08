da4bc0aa125151146d9d92537d925d392a003ee6
so_val <soyuz108@gmail.com>
2023-11-07 20:28:17 -0700

    Added a new challenge

---
+++ break_camelCase.js
@@ -0,0 +1,17 @@
+/* Complete the solution so that the function will break up camel casing, using a space between words. */
+
+// Classic function
+function breakCamel(str) {
+  let arr = Array.from(str);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i].toUpperCase()) {
+      arr[i] = " " + arr[i];
+    }
+  }
+  return arr.join("");
+}
+
+// Testing cases
+console.log(breakCamel("camelCasing")); // camel Casing
+console.log(breakCamel("identifier")); // identifier
+console.log(breakCamel("")); // ""
