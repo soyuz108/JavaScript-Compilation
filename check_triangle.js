cb4241ad52ea6405d0d788ec4b7de4bae55313d7
so_val <soyuz108@gmail.com>
2023-11-21 21:38:34 -0700

    Added a new challenge

---
+++ check_triangle.js
@@ -0,0 +1,12 @@
+/* Implement a function that accepts 3 integer values a, b, c. 
+The function should return true if a triangle can be built with the sides of given length and false in any other case. */
+
+function isTriangle(a, b, c) {
+  if (a && b && c) return a + b > c && b + c > a && a + c > b;
+  else return "One of the parameters is empty."
+}
+
+// Testing cases
+console.log(isTriangle(1, 2, 2)); // true
+console.log(isTriangle(7, 2, 2)); // false
+console.log(isTriangle(8, 5)); // One of the parameters is empty.
