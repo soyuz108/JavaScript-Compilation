630941635bb3be9b749fe146367c57a205581e2c
so_val <soyuz108@gmail.com>
2023-11-30 17:49:14 -0700

    Added a loop variant

--- order_by_inner_num.js
+++ order_by_inner_num.js
@@ -3,7 +3,6 @@ This number is the position the word should have in the result. Note: Numbers ca
 If the input string is empty, return an empty string. */
 
 // Using a helper function
-/*
 function order(words) {
   if (words) {
     let result = words.split(" ").sort((a, b) => getNum(a) - getNum(b));
@@ -15,12 +14,13 @@ function order(words) {
 // to use multiple times
 function getNum(word) {
   return Number(word.match(/\d+/g));
-} */
+}
 
 // Testing cases
 console.log(order("is2 Thi1s T4est 3a")); // Thi1s is2 3a T4est
 console.log(order("4of Fo1r pe6ople g3ood th5e the2")); // Fo1r the2 g3ood 4of th5e pe6ople
-console.log(order("")); ""
+console.log(order(""));
+("");
 console.log(order("3 6 4 2 8 7 5 1 9")); // 1 2 3 4 5 6 7 8 9
 
 // Simplified function
@@ -29,4 +29,20 @@ function order(words) {
     .split(" ")
     .sort((a, b) => a.match(/\d/) - b.match(/\d/))
     .join(" ");
+}
+
+// Classic function
+function order(words) {
+  const arr = words.split(" ");
+  let sortedArr = [];
+
+  for (i = 0; i <= arr.length; i++) {
+    for (j = 0; j < arr.length; j++) {
+      if (arr[j].indexOf(i) >= 0) {
+        sortedArr.push(arr[j]);
+      }
+    }
+  }
+
+  return sortedArr.join(" ");
 }
